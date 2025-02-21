5c10d88248a50d53afbddf7a234b446d81690433
confusedalex <alex@confusedalex.dev>
2025-02-21 20:52:35 +0100

    chore: remove commented out and duplicate code

--- home/rosetta.nix
+++ home/rosetta.nix
@@ -13,9 +13,6 @@
     ./features/desktop/common/firefox.nix
     ./features/desktop/common/intellij.nix
     ./features/desktop/common/emacs.nix
-    # ./features/desktop/common/jellyfin.nix
-    # ./features/desktop/common/josm.nix
-    # ./features/desktop/common/obsidian.nix
     ./features/desktop/common/office.nix
     ./features/desktop/common/school.nix
     ./features/desktop/common/signal.nix
@@ -26,27 +23,12 @@
     ./features/desktop/sway
     ./features/games/prismlauncher.nix
     ./features/productivity/mail.nix
-    # ./features/productivity/khal.nix
     ./global
   ];
-  programs = {
-    bash.enable = true; # Please read the comment before changing.
-    home-manager.enable = true;
 
-    thunderbird = {
-      enable = true;
-      profiles = {
-        "private" = {
-          isDefault = true;
-          withExternalGnupg = true;
-        };
-      };
-    };
-  };
   home = {
     stateVersion = "24.05";
     packages = with pkgs; [
-      rocketchat-desktop
       chromium
     ];
 
