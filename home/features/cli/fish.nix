968abf6b975803c7ee3a1a9286ae9598ccebdd0e
confusedalex <alex@confusedalex.dev>
2025-02-25 21:15:45 +0100

    chore: remove unused aliass

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -4,9 +4,6 @@
 
     shellAbbrs = {
       nd = "nix develop -c fish";
-      git-setwork = ''
-        git config user.name "Alexander Loll" && git config user.email a.loll@qvest-digital.com && git config gpg.format ssh && git config user.signingkey /home/alex/.ssh/work.pub
-      '';
       rm = "trash";
       grep = "rg";
     };
