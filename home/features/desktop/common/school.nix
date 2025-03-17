ad0401f28753058b1893a2358a44df9f3d25f319
confusedalex <alex@confusedalex.dev>
2025-03-17 17:18:21 +0100

    feat: add zotero

--- home/features/desktop/common/school.nix
+++ home/features/desktop/common/school.nix
@@ -5,10 +5,18 @@
     sqlitebrowser
     ciscoPacketTracer8
     anki
+    stable.zotero
   ];
 
   home.persistence."/persist/home/alex/".directories = [
+    # Anki
     ".local/share/Anki2"
+
+    # Packer Tracer
     "pt"
+
+    # Zotero
+    "Zotero"
+    ".zotero"
   ];
 }
