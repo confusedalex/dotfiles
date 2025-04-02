da7ec490200be2b24e102f247befecf8d0afac5b
confusedalex <alex@confusedalex.dev>
2025-04-02 20:45:35 +0200

    style: organise

--- home/features/desktop/common/intellij.nix
+++ home/features/desktop/common/intellij.nix
@@ -1,14 +1,19 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    pkgs.jetbrains.idea-community-bin
-  ];
-  home.persistence."/persist/home/alex/".directories = [
-    ".cache/JetBrains"
-    ".config/JetBrains"
-    ".jdks"
-    ".local/share/JetBrains"
-    ".m2"
-    ".gradle"
-  ];
+  home = {
+    packages = [
+      pkgs.jetbrains.idea-community-bin
+    ];
+    persistence."/persist/home/alex/" = {
+      directories = [
+        ".cache/JetBrains"
+        ".config/JetBrains"
+        ".jdks"
+        ".local/share/JetBrains"
+        ".m2"
+        ".gradle"
+      ];
+      files = [ ".ideavimrc" ];
+    };
+  };
 }
