a8ce76b9260be3828b8c19be3ab1a81797227e43
confusedalex <alex@confusedalex.dev>
2025-04-02 20:44:56 +0200

    feat: add mu and mu4e

--- home/features/desktop/common/emacs.nix
+++ home/features/desktop/common/emacs.nix
@@ -5,17 +5,25 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-pgtk;
-    extraPackages = epkgs: [ epkgs.treesit-grammars.with-all-grammars ];
+    extraPackages = epkgs: [
+      epkgs.treesit-grammars.with-all-grammars
+      epkgs.mu4e
+    ];
   };
 
   home.packages = with pkgs; [
     # LSPs
     python311Packages.python-lsp-server
+    # email
+    mu
     nixd
   ];
 
   home.persistence."/persist/home/alex/".directories = [
     ".emacs.d"
+
+    # mu
+    ".cache/mu"
 
     # Doom
     # ".config/emacs"
