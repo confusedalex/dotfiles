2fe2196d09cc184b7f109950a94b6947ebd02634
confusedalex <alex@confusedalex.dev>
2025-04-10 08:58:38 +0200

    fix: add persistence for vscode

--- home/features/desktop/common/code.nix
+++ home/features/desktop/common/code.nix
@@ -2,5 +2,8 @@
 {
   home.packages = [ pkgs.vscode-fhs ];
 
-  home.persistence."/persist/home/alex/".directories = [ ".vscode" ];
+  home.persistence."/persist/home/alex/".directories = [
+    ".vscode"
+    ".config/Code"
+  ];
 }
