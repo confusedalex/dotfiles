a52bdafb4a3042767df42bfb4e594b6b19622045
confusedalex <alex@confusedalex.dev>
2025-05-10 22:33:32 +0200

    feat: add ssh key to gaia

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -64,6 +64,7 @@
         port = 8192;
         authorizedKeys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC889TcvmQlCkDKZxz6p0VNN4mEFLzvSHUYDHPLg1PxxqncCUPdnuXvTx+ayf3gQmaI+gNCntJ3NMgB0p4/CfqVThMnN6SoqjVj32PxbKCV1JFQW1PcnS/iK4mZ/H2Vtm9uz5prM6bTUuxKCpczgpTpZS0b3VIxdm9J5hY7ou6xW6bAz25EA7mezupFh2QlIyu1FvgOrRaPQZs0ky0uljrdKOkDmtPgQ6Fj3wM2PkUrxuBGWav0SiQ4R9wuJw84lQ9+eB13kewfM+R8wusLY81whUgikDg1v1Fn+Nj1CmCuZg0Pxr73SFRR2nJqLQf56U7G0jCSF/e52OuRIqeusNpbAS8a5bSvs6PnCDV7N39OkAkAMiK4cjRtcZK40zVEYutaDoB/qVNhkeaFUAyvwMmKakE3WKORMUHOOkLqDUrxFeqhHirvg252lUSxDotXzQTx4FpXZa9uyFtwSeuiNl2pDMVN7feXh6D3dFdCHFtQXSvhLdzHmxNrqy747BX41HvNIAmPwBYUT0WVjlxHAmLlb+XUl1ue8vKw9+veicvNnBy49y0W38BWW+mMtOg+p+WeTKKkns8xTxAevRnTsCfJfqZym6NOLbtwr7iCqgd8nqAmKmOn/vYVXMEUCMjsnVYzoXlRiCCCONaDUlLpYnkyFoMysPD/GtzQ2p4w8GEsVw== cardno:19_764_596"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDCa78D6trTmSyyAwmf2w4uPx6Bhm7m7C3f2as6xZ4O alex@confusedalex.dev"
         ];
         hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
       };
