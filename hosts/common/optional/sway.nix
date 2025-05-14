61a96fb6d5a2914b1a3f0d9a448d14852f67ccd6
confusedalex <alex@confusedalex.dev>
2025-05-14 21:53:52 +0200

    fix(sway): set some env variables

--- hosts/common/optional/sway.nix
+++ hosts/common/optional/sway.nix
@@ -7,6 +7,11 @@
     wl-clipboard
   ];
 
+  environment = {
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
+  };
+
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
   services.gnome.gnome-keyring.enable = true;
