516ce1325e33c80ff259ff6d57b94e2f00986bed
confusedalex <alex@confusedalex.dev>
2025-05-21 10:41:18 +0200

    feat: add android-studio

--- home/rosetta.nix
+++ home/rosetta.nix
@@ -21,6 +21,7 @@
     ./features/desktop/common/signal.nix
     ./features/desktop/common/syncthing.nix
     ./features/desktop/common/wayland-wm
+    ./features/desktop/common/android-studio.nix
     ./features/desktop/common/vesktop.nix
     ./features/desktop/sway
     ./features/games/prismlauncher.nix
