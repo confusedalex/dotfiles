6d8ff5753a496d692ad051abcc1909034fa0334b
confusedalex <alex@confusedalex.dev>
2025-06-11 12:26:44 +0200

    refactor: simplify niri

--- hosts/common/optional/niri.nix
+++ hosts/common/optional/niri.nix
@@ -5,7 +5,8 @@
 {
   environment.systemPackages = with pkgs; [
     wl-clipboard
-	xwayland-satellite
+    xwayland-satellite
+    nautilus
   ];
 
   environment = {
@@ -16,31 +17,9 @@
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
   services.dbus.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
-  security.pam.services.greetd.enableGnomeKeyring = true;
-
-  xdg.portal = {
-    enable = true;
-    configPackages = [ pkgs.niri ];
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-gnome
-    ];
-  };
 
   # enable niri
   programs.niri.enable = true;
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'niri-session'";
-        user = "greeter";
-      };
-    };
-  };
 
   security.pam.services.swaylock = { };
 }
